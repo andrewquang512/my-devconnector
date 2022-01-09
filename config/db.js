e1b7aa437dfb15c63c58b2c8fa2d9be3b7aa474e
andrewquang <andrew.viquang@gmail.com>
2022-01-09 15:15:22 +0700

    init routes

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoURI')
+
+// ? new standard now is sync await, use when it is synchronous
+// not this mongoose.connect(db)
+
+const connectDB = async () => {
+  //? practice using try catch
+  try {
+    // await mongoose.connect(db) or under method are both correct
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    })
+    console.log('MongoDB Connected...')
+  } catch (error) {
+    console.error(error.message)
+    process.exit(1)
+    // ? exit process with failure (1), success(0)
+  }
+}
+
+module.exports = connectDB
